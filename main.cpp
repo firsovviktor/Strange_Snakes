8d2f5aa3c473baeaf3f823815941987d0fa60175
firsovviktor <72372722+firsovviktor@users.noreply.github.com>
2021-04-30 19:26:05 +0300

    +Interface and Difiiculties
    main.cpp of final (I hope) version of the game.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #define PI 3.14159265
 int AspectRatio = 4;
 float Snake_speed = 0.01;
+bool invisible = true;
 using namespace std;
 using namespace sf;
 
@@ -68,6 +69,7 @@ ConvexShape draw_tile(const Tile& figure){
     convex.setOutlineColor(figure.countour);
     return convex;
 }
+
 RectangleShape draw_snake(const Snake& s, int i, int step){
     RectangleShape r;
     float dist2 = (s.vert[i+1].x-s.vert[i].x)*(s.vert[i+1].x-s.vert[i].x) + (s.vert[i+1].y-s.vert[i].y)*(s.vert[i+1].y-s.vert[i].y);
@@ -84,6 +86,10 @@ RectangleShape draw_snake(const Snake& s, int i, int step){
     r.setFillColor(s.inside);
     r.setOutlineThickness(s.thick);
     r.setOutlineColor(s.countour);
+    if ((sqrt((s.vert[i+1].x-s.vert[i].x)*(s.vert[i+1].x-s.vert[i].x) + (s.vert[i+1].y-s.vert[i].y)*(s.vert[i+1].y-s.vert[i].y)) > step * 6)&&(invisible == true)){
+        r.setFillColor({0, 0, 0, 0});
+        r.setOutlineColor({0, 0, 0, 0});
+    }
     return r;
 }
 RectangleShape draw_snake_head(const Snake& s, int step, float time){
@@ -104,6 +110,10 @@ RectangleShape draw_snake_head(const Snake& s, int step, float time){
     r.setFillColor(s.inside_head);
     r.setOutlineThickness(s.thick_head);
     r.setOutlineColor(s.countour_head);
+    if ((sqrt((s.vert[i+1].x-s.vert[i].x)*(s.vert[i+1].x-s.vert[i].x) + (s.vert[i+1].y-s.vert[i].y)*(s.vert[i+1].y-s.vert[i].y)) > step * 6)&&(invisible==true)){
+        r.setFillColor({0, 0, 0, 0});
+        r.setOutlineColor({0, 0, 0, 0});
+    }
     return r;
 }
 RectangleShape draw_snake_tail(const Snake& s, int step, float time){
@@ -127,8 +137,13 @@ RectangleShape draw_snake_tail(const Snake& s, int step, float time){
     r.setFillColor(s.inside);
     r.setOutlineThickness(s.thick);
     r.setOutlineColor(s.countour);
+    if ((sqrt((s.vert[i+1].x-s.vert[i].x)*(s.vert[i+1].x-s.vert[i].x) + (s.vert[i+1].y-s.vert[i].y)*(s.vert[i+1].y-s.vert[i].y)) > step * 6)&&(invisible == true)){
+        r.setFillColor({0, 0, 0, 0});
+        r.setOutlineColor({0, 0, 0, 0});
+    }
     return r;
 }
+
 vector<Tile> gen_square_field(int Width, int Height, int step, Tile t){
     vector<Tile> field;
     for (int i=0; i<Width/step; i++){
@@ -196,6 +211,7 @@ vector<Tile> near_sq_field(vector<Tile> field, int Height, int Width, int step){
     }
     return field;
 }
+
 vector<Tile> gen_triangle_field (int Width, int Height, int step, Tile tile){
     int a = Width/step - 1;
     int r = Height/(sqrt(3)*step);
@@ -326,6 +342,7 @@ vector<Tile> near_tr_field(vector<Tile> field, int Height, int Width, int step){
     }
     return field;
 }
+
 vector<Tile> gen_hex_field(int Width, int Height, int step, Tile t, int r, int a){
     float f = sqrt(3)/6;
     vector<Tile> field;
@@ -491,6 +508,7 @@ vector<Tile> near_hex_field(vector<Tile> field, int Height, int Width, int step,
     }
     return field;
 }
+
 bool check_if_dead (Snake s){
     for (int i=0; i<s.vert.size(); i++){
         for (int j=i; j<s.vert.size(); j++){
@@ -513,10 +531,12 @@ void win_screen(int score){
 int main() {
     srand(time(NULL));
     // Устанавливаем 8-й уровень сглаживания
-    string type_of_field = "Hex";
-    const int Width = 1000, Height = 800, step = 40;
+    bool interface = true;
+    string type_of_field = "Square";
+    int Width = 1000, Height = 800, step = 40;
     Tile Prototype({0, 0}, {255, 1, 1, 255}, {1, 255, 1, 50}, 1, {{10, 30}, {10, 100}, {50, 70}}, {});
     Snake s;
+
     s.countour = {128, 1, 128, 255};
     s.countour_head = {1, 1, 255, 255};
     s.inside = {1, 255, 1, 255};
@@ -533,15 +553,236 @@ int main() {
     float fl = Width/sqrt(3);
     int a = floor(fl)/step - 1;
 
+    string Difficulty = "None";
+
+    if (interface){
+
+        Font font;
+
+        if (!font.loadFromFile("arial.ttf"))
+        {
+            // error...
+        }
+
+        RectangleShape sandbox, normal, hard, set, start;
+        sandbox.setOutlineColor({0, 0, 0, 255});
+        normal.setOutlineColor({0, 0, 0, 255});
+        hard.setOutlineColor({0, 0, 0, 255});
+        set.setOutlineColor({0, 0, 0, 255});
+        start.setOutlineColor({0, 0, 0, 255});
+
+        sandbox.setOutlineThickness(5);
+        normal.setOutlineThickness(5);
+        hard.setOutlineThickness(5);
+        set.setOutlineThickness(5);
+        start.setOutlineThickness(5);
+
+        sandbox.setFillColor({0, 188, 0, 100});
+        normal.setFillColor({0, 0, 188, 100});
+        hard.setFillColor({188, 0, 0, 100});
+        set.setFillColor({0, 188, 0, 100});
+        start.setFillColor({0, 188, 0, 100});
+
+        sandbox.setSize({180, 60});
+        sandbox.setPosition(235, 160);
+
+        normal.setSize({160, 60});
+        normal.setPosition(430, 160);
+
+        hard.setSize({160, 60});
+        hard.setPosition(605, 160);
+
+        set.setSize({680, 60});
+        set.setPosition(60, 310);
+
+        start.setSize({280, 100});
+        start.setPosition(250, 440);
+
+        Text text;
+        text.setFont(font);
+        text.setString("Hello world");
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::Red);
+        text.setStyle(sf::Text::Bold);
+
+        RenderWindow interface(VideoMode(800, 600), "Settings screen!", Style::Default, settings);
+        interface.clear(Color::White);
+
+        string Button_pressed;
+
+        int x, y;
+
+        while(interface.isOpen()){
+            Event event;
+            while (interface.pollEvent(event)) {
+                if (event.type == Event::Closed){
+                    interface.close();
+                }
+
+                if (event.type == Event::MouseButtonPressed){
+                    //cout << event.mouseButton.x << " " << event.mouseButton.y << endl;
+                    x = event.mouseButton.x;
+                    y = event.mouseButton.y;
+                }
+            }
+
+            interface.clear(Color::White);
+
+            Button_pressed = "None";
+            if ((x>=235)&&(x<=235+180)){
+                if ((y>=160)&&(y<=160+60)){
+                    Button_pressed = "Sandbox";
+                    Difficulty = "Sandbox";
+                }
+            }
+            if ((x>=430)&&(x<=430+160)){
+                if ((y>=160)&&(y<=160+60)){
+                    Button_pressed = "Normal";
+                    Difficulty = "Normal";
+                }
+            }
+
+            if ((x>=605)&&(x<=605+160)){
+                if ((y>=160)&&(y<=160+60)){
+                    Button_pressed = "Hard";
+                    Difficulty = "Hard";
+                }
+            }
+
+            if ((x>=60)&&(x<=60+680)){
+                if ((y>=310)&&(y<=310+60)){
+                    Button_pressed = "set";
+                }
+            }
+
+            if ((x>=250)&&(x<=250+280)){
+                if ((y>=440)&&(y<=440+100)){
+                    Button_pressed = "start";
+                }
+            }
+
+            if (Difficulty!="None"){
+                sandbox.setFillColor({0, 188, 0, 100});
+                normal.setFillColor({0, 0, 188, 100});
+                hard.setFillColor({188, 0, 0, 100});
+
+                if (Difficulty == "Sandbox"){
+                    sandbox.setFillColor({0, 255, 0, 255});
+                }
+
+                if (Difficulty == "Normal"){
+                    normal.setFillColor({0, 0, 255, 255});
+                }
+
+                if (Difficulty == "Hard"){
+                    hard.setFillColor({255, 0, 0, 255});
+                }
+            }
+
+            if (Button_pressed == "set"){
+                Difficulty = "set";
+                interface.close();
+            }
+
+            if (Button_pressed == "start"){
+                interface.close();
+            }
+
+            interface.draw(sandbox);
+            interface.draw(normal);
+            interface.draw(hard);
+            interface.draw(set);
+
+            interface.draw(start);
+            text.setString("SETTINGS");
+            text.setCharacterSize(60);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(230, 30);
+            interface.draw(text);
+
+            text.setString("Difficulty:");
+            text.setCharacterSize(40);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(20, 160);
+            interface.draw(text);
+
+            text.setString("Sandbox");
+            text.setCharacterSize(40);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(240, 160);
+            interface.draw(text);
+
+            text.setString("Normal");
+            text.setCharacterSize(40);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(440, 160);
+            interface.draw(text);
+
+            text.setString("Hard");
+            text.setCharacterSize(40);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(640, 160);
+            interface.draw(text);
+
+            text.setString("Advanced settings through console");
+            text.setCharacterSize(40);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(70, 310);
+            interface.draw(text);
+
+            text.setString("START");
+            text.setCharacterSize(80);
+            text.setFillColor({0, 0, 0, 255});
+            text.setPosition(260, 440);
+            interface.draw(text);
+
+            interface.display();
+        }
+    }
+
+    if (Difficulty == "None"){
+        Difficulty = "Sandbox";
+    }
+
+    if (Difficulty == "set"){
+
+        cout << "You have chosen to open an advanced settings." << endl;
+        cout << "Enter resolution with a space between. Width first, then height." << endl;
+        cout << "800 800 or more is recommended." << endl;
+        cin >> Width >> Height;
+
+        cout << "Chose size of a tile. 40 is recommednded." << endl;
+        cin >> step;
+
+        cout << "Chose type of filed. Square/Triangle/Hex." << endl;
+        cin >> type_of_field;
+
+        cout << "Chose your difficulty. Sandbox/Normal/Hard" << endl;
+        cin >> Difficulty;
+
+        cout << "Chose initial speed of the snake. 0.002 - 0.05 is recommended." << endl;
+        cin >> Snake_speed;
+
+        cout << "Chose ratio of tile size to the snake size (an integer). 4 is recommended" << endl;
+        cin >> AspectRatio;
+
+        cout << "Chose if you want the snake to be invisible while going over the edge of the board (0/1). 1 is recommended" << endl;
+        cin >> invisible;
+    }
+
+    cout << "Starting a game" << endl;
     // Объект, который, собственно, является главным окном приложения
+
     RenderWindow window(VideoMode(Width, Height), "SFML Works!", Style::Default, settings);
     window.clear(Color::White);
 
     vector<Tile> field;
+
     if (type_of_field == "Square"){
         field = gen_square_field(Width, Height, step, Prototype);
         field = near_sq_field(field, Height, Width, step);
     }
+
     if (type_of_field == "Triangle"){
         field = gen_triangle_field(Width, Height, step, Prototype);
         field = near_tr_field(field, Height, Width, step);
@@ -558,7 +799,9 @@ int main() {
     s.vert.push_back(field[0].center);
     s.vert.push_back(field[1].center);
     s.vert.push_back(field[2].center);
+
     //s.vert.push_back(field[3].center);
+
     Tile cur = field[2];
 
     //cout << "snake initialized" << endl;
@@ -567,12 +810,15 @@ int main() {
     if (type_of_field == "Square"){
         key = 1;
     }
+
     if (type_of_field == "Triangle"){
         key = 2;
     }
+
     if (type_of_field == "Hex"){
         key = 3;
     }
+
     bool charged = false;
 
     //cout << "before apple initialization" << endl;
@@ -588,6 +834,7 @@ int main() {
     //cout << "Window is starting now" << endl;
 
     while (window.isOpen()) {
+
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
@@ -661,7 +908,13 @@ int main() {
                 charged = true;
             }
         }
+
         s.time += Snake_speed;
+
+        if (Difficulty == "Hard"){
+            Snake_speed += 1e-7;
+        }
+
         if (s.time >= 1){
             //cout << "Snake Head is now at: " << cur.center.x << "/" << cur.center.y << endl;
 
@@ -676,6 +929,9 @@ int main() {
                 s.vert.erase(s.vert.begin());
             }else{
                 charged = false;
+                if (Difficulty == "Hard"){
+                    Snake_speed += 1e-3;
+                }
             }
         }
 
@@ -715,9 +971,20 @@ int main() {
             window.draw(circle);
             //cout << ap.tile.center.x << ' ' << ap.tile.center.y << endl;
         }
+
         if (check_if_dead(s)){
             cout << "You have died(" << endl;
+            if (Difficulty=="Normal"){
+                cout << "Your score is: " << s.vert.size();
+                window.close();
+            }
+
+            if (Difficulty=="Hard"){
+                cout << "Your score is: " << s.vert.size();
+                window.close();
+            }
         }
+
         window.draw(draw_snake_head(s, step, s.time));
 
         for (int i=1; i<s.vert.size()-2; i++){
@@ -726,6 +993,7 @@ int main() {
 
         window.draw(draw_snake_tail(s, step, s.time));
         window.display();
+
     }
 
     return 0;
